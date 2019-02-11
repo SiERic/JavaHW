e3ea31ee64c314035b33dc4b665db953460e75aa
Alexandra Olemskaya <alexandra.olemskaya@gmail.com>
2019-02-12 00:11:59 +0300

    add some information to javadocs

--- hw2-trie/src/main/java/me/sieric/trie/Trie.java
+++ hw2-trie/src/main/java/me/sieric/trie/Trie.java
@@ -38,7 +38,12 @@ public class Trie implements Serializable {
             return go.get(symbol);
         }
 
-        /** Serialize Node into OutputStream */
+
+        /**
+         * Serialize Node into OutputStream
+         * @param out - OutputStream to write into
+         * @throws IOException if writing into stream failed
+         */
         private void serialize(OutputStream out) throws IOException {
             ObjectOutputStream stream = new ObjectOutputStream(out);
             stream.writeBoolean(isTerminal);
@@ -53,7 +58,11 @@ public class Trie implements Serializable {
             stream.flush();
         }
 
-        /** Deserialize Node from InputStream */
+        /**
+         * Deserialize Node from InputStream
+         * @param in - InputStream to read from
+         * @throws IOException if reading from stream failed
+         */
         private void deserialize(InputStream in) throws IOException {
             ObjectInputStream stream = new ObjectInputStream(in);
             isTerminal = stream.readBoolean();
@@ -79,6 +88,7 @@ public class Trie implements Serializable {
     /**
      * Adds a new string to Trie.
      * Returns true if string already exists, false otherwise
+     * @return true if string already exists, false otherwise
      */
     public boolean add(@NotNull String element) {
         return addFromNode(root, element, 0, true);
@@ -88,6 +98,11 @@ public class Trie implements Serializable {
      * Adds/Removes (Adds if toAdd, Removes otherwise)
      * a new string element[index:] to the subtree of given node.
      * Return true, if the operation is correct
+     * @param node - a current node, where the path starts
+     * @param element - a string, which suffix to add/remove from trie
+     * @param index - a position from which the suffix starts
+     * @param valueAtTheEndOfThePath - a flag, which is true if suffix is adding to Trie and false if deleting
+     * @return true if a new string has been added/removed or false otherwise
      */
     private boolean addFromNode(Node node, String element, int index, boolean valueAtTheEndOfThePath) {
         if (element.length() == index) {
@@ -107,7 +122,11 @@ public class Trie implements Serializable {
         }
     }
 
-    /** Checks if Trie contains a string */
+    /**
+     * Checks if Trie contains a string
+     * @param element - a string for which to check whether it is in Trie
+     * @return true if the given string is in the Trie, false otherwise
+     */
     public boolean contains(@NotNull String element) {
         Node currentNode = root;
         for (int i = 0; i < element.length(); i++) {
@@ -119,17 +138,25 @@ public class Trie implements Serializable {
     /**
      * Removes string from Trie.
      * Returns true, if the string existed in the Trie before removal, false otherwise
+     * @param element - a string to remove
+     * @return true, if the string existed in the Trie before removal, false otherwise
      */
     public boolean remove(@NotNull String element) {
         return addFromNode(root, element, 0, false);
     }
 
-    /** Gets a number of strings in Trie */
+    /**
+     * Gets a number of strings in Trie
+     * @return number of strings in Trie
+     */
     public int size() {
         return root.size;
     }
 
-    /** Returns number of Strings in the Trie which start with given prefix */
+    /**
+     * Returns number of strings in the Trie which start with given prefix
+     * @return  number of strings in the Trie which start with given prefix
+     */
     public int howManyStartWithPrefix(@NotNull String prefix) {
         Node currentNode = root;
         for (int i = 0; i < prefix.length(); i++) {
@@ -138,12 +165,20 @@ public class Trie implements Serializable {
         return currentNode.size;
     }
 
-    /** Serialize Trie into OutputStream */
+    /**
+     * Serialize Trie into OutputStream
+     * @param out - OutputStream to write into
+     * @throws IOException if writing into stream failed
+     */
     void serialize(OutputStream out) throws IOException {
         root.serialize(out);
     }
 
-    /**  Deserialize Node from InputStream */
+    /**
+     * Deserialize Node from InputStream
+     * @param in - InputStream to read from
+     * @throws IOException if reading from stream failed
+     */
     void deserialize(InputStream in) throws IOException {
         root.deserialize(in);
     }
